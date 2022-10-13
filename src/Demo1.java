4a5ec4094b9c3a05c84cbc4b1751c9518aa1107f
Soumy17 <pratikmishra738@gmail.com>
2022-10-13 12:09:08 +0530

    First commit

---
+++ src/Demo1.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class demo1
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+	
+	int testcases=sc.nextInt();
+	
+	for(int i=1 ;i<=testcases;i++)
+	{
+	    int no_of_elements=sc.nextInt();
+	      
+	 
+	}
+	
+}
+}
